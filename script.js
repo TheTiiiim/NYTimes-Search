b6395fc55fd116d5988246d806c71d6c44015d7a
Tim Scott <0timothy.scott0@gmail.com>
2021-01-23 13:51:26 -0500

    seach additional pages

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(function () {
 	$("form#searchForm").on("submit", function (e) {
 		e.preventDefault();
+		clearResults();
 
 		let searchTerm = $("#searchTerm").val();
 
 		let numRecords = parseInt($("#numRecords").val());
+
+		let numPages = Math.ceil(numRecords / 10);
 
 		let beginDate = $("#startYear").val();
 		if (beginDate) {
@@ -18,39 +21,41 @@ $(function () {
 			endDate = "&end_date=" + endDate;
 		}
 
-		// console.log(beginDate)
 
-		let queryString =
-			"https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
-			+ searchTerm
-			+ beginDate
-			+ endDate
-			+ "&sort=relevance"
-			+ "&api-key=N91g2FybbqotXrcwqlFCzGhdDQnNFcXT";
+		for (let pageN = 0; pageN < numPages; pageN++) {
+			let queryString =
+				"https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+				+ searchTerm
+				+ beginDate
+				+ endDate
+				+ "&page=" + pageN
+				+ "&sort=relevance"
+				+ "&api-key=N91g2FybbqotXrcwqlFCzGhdDQnNFcXT";
 
-		$.get(queryString)
-			.then(function (data) {
-				let docs = data.response.docs;
+			$.get(queryString)
+				.then(function (data) {
+					let docs = data.response.docs;
 
-				console.log(docs)
+					console.log(docs)
 
-				let resultsArea = $(".searchResults");
+					let resultsArea = $(".searchResults");
 
-				for (let i = 0; i < numRecords; i++) {
-					resultsArea.append(
-						$("<div>").append(
-							$("<a>")
-								.attr("href", docs[i].web_url)
-								.text(docs[i].headline.main)
-						)
-					);
-				}
-			})
+					for (let i = 0; i < numRecords; i++) {
+						resultsArea.append(
+							$("<div>").append(
+								$("<a>")
+									.attr("href", docs[i].web_url)
+									.text(docs[i].headline.main)
+							)
+						);
+					}
+				})
+		}
 	});
 
-	$(".reset").on("click", function (e) {
+	$(".reset").on("click", clearResults);
+
+	function clearResults() {
 		$(".searchResults").empty();
-	});
-
-
+	}
 });
